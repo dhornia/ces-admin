91acd6a3bd9b5944fb1cd979a0afda55b95ba262
Darlen Hornia Puentes <hornia84@gmail.com>
2026-05-30 12:41:33 -0300

    Encapsulate User fields and add getters/setters

--- src/model/User.java
+++ src/model/User.java
@@ -1,17 +1,57 @@
 package model;
 
 public class User {
-    String name;
-    String lastname;
-    String email;
-    String password;
-    String country;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String country;
 
-    public User(String name, String lastname, String email, String password, String country) {
+    public User(String name, String lastName, String email, String password, String country) {
         this.name = name;
-        this.lastname = lastname;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
         this.country = country;
     }
 
