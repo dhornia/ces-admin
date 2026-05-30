5e377c56b11e9115c6195cac90f779d7d70988a6
Darlen Hornia Puentes <hornia84@gmail.com>
2026-05-30 15:15:51 -0300

    Add register method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import model.User;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+
+        Map<String, User> users = new HashMap<>();
 
         User user = new User(
                 "Darlen",
@@ -14,5 +19,21 @@ public class Main {
         System.out.println(
                 user.login("darlen@gmail.com", "1234")
         );
+
+        System.out.println(register(user, users));
+
+        System.out.println(register(user, users));
+
+        System.out.println(users);
+    }
+
+    public static boolean register(User user, Map<String, User> users) {
+
+        if (users.containsKey(user.getEmail())) {
+            return false;
+        }
+
+        users.put(user.getEmail(), user);
+        return true;
     }
 }
