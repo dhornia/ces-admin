ec57cbf83a217dd2d7c08d6f79da4e29cc876fff
Darlen Hornia Puentes <hornia84@gmail.com>
2026-06-06 18:43:58 -0300

    Add tester class

---
+++ src/model/Tester.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Tester extends User {
+    private String level;
+
+    public Tester(String name, String lastName, String email, String password, String country, String level) {
+        super(name, lastName, email, password, country);
+        this.level = level;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
