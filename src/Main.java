ad959673dc0e1f506b32fe4fb6711ecf7fff81e0
Darlen Hornia Puentes <hornia84@gmail.com>
2026-06-21 20:53:53 -0300

    Refactor

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import service.UserSystem;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserSystem userSystem = new UserSystem();
@@ -13,97 +14,170 @@ public class Main {
     }
 
     private static void showMenu(Scanner scanner, UserSystem userSystem) {
+
         String option = "0";
 
         while (!option.equals("4")) {
-            System.out.println();
+
+            System.out.println("\n=====================");
+            System.out.println("     Admin CES");
             System.out.println("=====================");
-            System.out.println("     Sistema Usuario     ");
-            System.out.println("=====================");
 
-            System.out.println("1 - Registrar usuario");
-            System.out.println("2 - Login");
-            System.out.println("3 - Listar usuarios");
-            System.out.println("4 - Salir");
-
-            System.out.println("---------------------");
-            System.out.print("Seleccione una opción: ");
-
-            option = scanner.nextLine();
-
-            switch (option) {
-                case "1":
-                    System.out.println("\nRegistro de usuario ...");
-
-                    System.out.print("Nombre: ");
-                    String name = scanner.nextLine();
-
-                    System.out.print("Apellido: ");
-                    String lastName = scanner.nextLine();
-
-                    System.out.print("Correo: ");
-                    String email = scanner.nextLine();
-
-                    System.out.print("Contraseña: ");
-                    String password = scanner.nextLine();
-
-                    System.out.print("País: ");
-                    String country = scanner.nextLine();
-
-                    if (name.isBlank() || lastName.isBlank() || email.isBlank()
-                            || password.isBlank() || country.isBlank()) {
-                        System.out.println("Todos los campos son obligatorios");
-                        return;
-                    }
-
-                    User user = new User(
-                            name,
-                            lastName,
-                            email,
-                            password,
-                            country
-                    );
-
-                    if (userSystem.registerUser(user)) {
-                        System.out.println("Usuario registrado.");
-                    } else {
-                        System.out.println("Ya existe un usuario registrado con este correo.");
-                    }
-
-                    break;
-
-                case "2":
-                    System.out.println("\nLogin ...");
-
-                    System.out.print("Correo: ");
-                    String loginEmail = scanner.nextLine();
-
-                    System.out.print("Contraseña: ");
-                    String loginPassword = scanner.nextLine();
-
-                    if (userSystem.login(loginEmail, loginPassword)) {
-                        System.out.println("Login exitoso.");
-                    } else {
-                        System.out.println("Credenciales inválidas.");
-                    }
-
-                    break;
-
-                case "3":
-                    System.out.println("\nLista de Usuarios ...");
-
-                    userSystem.getAllUsers();
-                    break;
-
-                case "4":
-                    System.out.println("\nSaliendo ...");
-
-                    System.out.println("Hasta luego!");
-                    break;
-
-                default:
-                    System.out.println("Opción no válida");
+            if (userSystem.getCurrentUser() == null) {
+                option = showPublicMenu(scanner, userSystem);
+            } else {
+                option = showAdminMenu(scanner, userSystem);
             }
+        }
+    }
+
+    private static String showPublicMenu(Scanner scanner, UserSystem userSystem) {
+
+        System.out.println("1 - Iniciar Sesión");
+        System.out.println("2 - Registrarse");
+        System.out.println("3 - Salir");
+
+        System.out.print("Seleccione una opción: ");
+        String option = scanner.nextLine();
+
+        switch (option) {
+
+            case "1":
+                login(scanner, userSystem);
+                break;
+
+            case "2":
+                registerAdmin(scanner, userSystem);
+                break;
+
+            case "3":
+                System.out.println("\nSaliendo ...");
+                System.out.println("Hasta luego!");
+                break;
+
+            default:
+                System.out.println("Opción no válida");
+        }
+
+        return option;
+    }
+
+    private static String showAdminMenu(Scanner scanner, UserSystem userSystem) {
+
+        System.out.println("\n*** Bienvenido " + userSystem.getCurrentUser().getName() + " " + userSystem.getCurrentUser().getLastName() + " ***");
+        System.out.println("--- MENU ---");
+
+        System.out.println("1 - Buscar usuario");
+        System.out.println("2 - Ver usuarios");
+        System.out.println("3 - Cerrar sesión");
+
+        System.out.print("Seleccione una opción: ");
+        String option = scanner.nextLine();
+
+        switch (option) {
+            case "1":
+                findUser(scanner, userSystem);
+                break;
+
+            case "2":
+                listUsers(userSystem);
+                break;
+
+            case "3":
+                userSystem.logout();
+                option = "0";
+                break;
+        }
+
+        return option;
+    }
+
+    private static void login(Scanner scanner, UserSystem userSystem) {
+
+        System.out.println("\nIniciar Sesión Administrador ...");
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Contraseña: ");
+        String password = scanner.nextLine();
+
+        User user = userSystem.login(email, password);
+
+        if (user != null) {
+            System.out.println("Login exitoso.");
+        } else {
+            System.out.println("Credenciales inválidas.");
+        }
+    }
+
+    private static void registerAdmin(Scanner scanner, UserSystem userSystem) {
+
+        System.out.println("\nCrear Cuenta Administrador ...");
+
+        System.out.print("Nombre: ");
+        String adminName = scanner.nextLine();
+
+        System.out.print("Apellido: ");
+        String adminLastName = scanner.nextLine();
+
+        System.out.print("Correo: ");
+        String adminEmail = scanner.nextLine();
+
+        System.out.print("Contraseña: ");
+        String adminPassword = scanner.nextLine();
+
+        System.out.print("País: ");
+        String adminCountry = scanner.nextLine();
+
+        if (adminName.isBlank() || adminLastName.isBlank() || adminEmail.isBlank()
+                || adminPassword.isBlank() || adminCountry.isBlank()) {
+            System.out.println("Todos los campos son obligatorios");
+            return;
+        }
+
+        if (userSystem.registerUser(adminName, adminLastName, adminEmail, adminPassword, adminCountry)) {
+            System.out.println("Usuario registrado.");
+        } else {
+            System.out.println("Ya existe un usuario registrado con este correo.");
+        }
+    }
+
+    private static void findUser(Scanner scanner, UserSystem userSystem) {
+
+        System.out.print("Email: ");
+        String searchEmail = scanner.nextLine();
+
+        User user = userSystem.findUserByEmail(searchEmail);
+
+        if (user == null) {
+            System.out.println("Usuario no encontrado");
+        } else {
+            System.out.println("\n--- INFORMACIÓN DEL USUARIO ---");
+            System.out.println("Nombre: " + user.getName());
+            System.out.println("Apellido: " + user.getLastName());
+            System.out.println("País: " + user.getCountry());
+            System.out.println("Perfil: " + user.getRole());
+        }
+    }
+
+    private static void listUsers(UserSystem userSystem) {
+
+        System.out.println("# - Nombre - Correo - País de nacimiento - Perfil");
+        System.out.println("--------------------------------------");
+
+        int i = 1;
+
+        for (User u : userSystem.getAllUsers()) {
+            System.out.println(
+                    i + " - " +
+                            u.getName() + " " +
+                            u.getLastName() + " - " +
+                            u.getEmail() + " - " +
+                            u.getCountry() + " - " +
+                            u.getRole()
+            );
+            i++;
         }
     }
 }
--- src/service/UserSystem.java
+++ src/service/UserSystem.java
@@ -1,63 +1,72 @@
 package service;
 
+import model.Admin;
+import model.Tester;
 import model.User;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserSystem {
+
     private Map<String, User> users;
+    private User currentUser;
 
     public UserSystem() {
         this.users = new HashMap<>();
-        loadTestUsers();
+        loadTestUsersData();
     }
 
-    public boolean registerUser(User user) {
-        if (userExists(user.getEmail())) {
+    public User login(String email, String password) {
+        User user = users.get(email);
+
+        if (user != null && user.getPassword().equals(password)) {
+            currentUser = user;
+            return user;
+        }
+
+        return null;
+    }
+
+    public void logout() {
+        currentUser = null;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public boolean registerUser(String name, String lastName, String email, String password, String country) {
+        if (userExists(email)) {
             return false;
         }
 
-        users.put(user.getEmail(), user);
+        User user = new Admin(name, lastName, email, password, country);
+        users.put(email, user);
         return true;
     }
 
-    public boolean login(String email, String password) {
-        User user = findUserByEmail(email);
-
-        return user != null &&
-                user.getPassword().equals(password);
+    public Collection<User> getAllUsers() {
+        return users.values();
     }
 
-    public void getAllUsers() {
-        System.out.println("# - Nombre - Correo - País de nacimiento");
-        System.out.println("--------------------------------------");
-
-        int i = 1;
-
-        for (User user : users.values()) {
-            System.out.println(
-                    i + " - " +
-                            user.getName() + " " +
-                            user.getLastName() + " - " +
-                            user.getEmail() + " - " +
-                            user.getCountry()
-            );
-            i++;
-        }
+    public User findUserByEmail(String email) {
+        return users.get(email);
     }
 
     private boolean userExists(String email) {
         return users.containsKey(email);
     }
 
-    private User findUserByEmail(String email) {
-        return users.get(email);
-    }
+    private void loadTestUsersData() {
+        users.put("admin@test.com",
+                new Admin("Fiorella", "Lopez", "admin@test.com", "1234", "UY"));
 
-    private void loadTestUsers() {
-        users.put("admin@test.com", new User("Fiorella", "Lopez", "admin@test.com", "1234", "UY"));
-        users.put("user1@test.com", new User("Juan", "Furtado", "user1@test.com", "1234", "UY"));
-        users.put("user2@test.com", new User("Viviana", "Caprani", "user2@test.com", "1234", "CU"));
+        users.put("user1@test.com",
+                new Tester("Juan", "Furtado", "user1@test.com", "1234", "UY", "Tester Junior"));
+
+        users.put("user2@test.com",
+                new Tester("Viviana", "Caprani", "user2@test.com", "1234", "CU", "Tester Senior"));
     }
 }
